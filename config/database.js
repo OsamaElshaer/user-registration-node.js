3ff3fcee68f2f596e0b140b6d1c47269e8ec9465
osamaelshaer <osamaelshaer944@gmail.com>
2022-10-09 14:30:15 +0200

    config mongodb

---
+++ config/database.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const { MongoClient } = require("mongodb");
+const url = process.env.URL_DB;
+const dbName = "userManagment";
+const clien = new MongoClient(url);
+
+let database;
+
+async function dbConnection(cb) {
+  await clien.connect();
+  database = clien.db(dbName);
+  cb(database)
+}
+
+
+
+
+function db() {
+  if (!database) {
+    return new Error("can not reach db");
+  }
+  return database;
+}
+
+module.exports={
+    dbConnection,
+    db
+}
--- config/passport.js
+++ config/passport.js
@@ -1,24 +1,13 @@
-// The “done()” function is then used to pass the “{authenticated_user}” to the serializeUser() function.
-
-// Note, the done(<err>, <user>) function in the “authUser” is passed as ,
-
-// 1. If the user not found in DB,
-// done (null, false)
-
-// 2. If the user found in DB, but password does not match,
-// done (null, false)
-
-// 3. If user found in DB and password match,
-// done (null, {authenticated_user})
-
-const { findBy } = require("../models/auth");
+const { User } = require("../models/User");
 
 exports.authUser = (username, password, done) => {
-  const user = findBy("username", username);
-
-  if (!user) return done(null, false, { message: "user not found" });
-  if (user.password !== password)
-    return done(null, false, { message: "wrong password" });
-
-  return done(null, user);
+  const user = User.findByName(username, (user) => {
+    if (!user) {
+      return done(null, false, { message: "user not found" });
+    }
+    if (user.password !== password) {
+      return done(null, false, { message: "wrong password" });
+    }
+    return done(null, user);
+  });
 };
