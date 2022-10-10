7bb6d4a37e980b3264cd31eba2b5ef766f4cd19c
osamaelshaer <osamaelshaer944@gmail.com>
2022-10-10 12:45:44 +0200

    express validator

--- routes/auth.js
+++ routes/auth.js
@@ -1,20 +1,64 @@
-const router = require("express").Router();
-const { logIn, index, logOut, signUP, postSignUp } = require("../controllers/auth");
-const { checkAuthenticated, checkLoggedIn } = require("../middleware/isAuth");
 const passport = require("passport");
+const { body } = require("express-validator");
+const { db } = require("../config/database");
+const router = require("express").Router();
+
+const {
+  logIn,
+  postLogIn,
+  index,
+  logOut,
+  signUP,
+  postSignUp,
+} = require("../controllers/auth");
+
+const { checkAuthenticated, checkLoggedIn } = require("../middleware/isAuth");
 
 router
   .route("/login")
   .get(logIn)
   .post(
+    body("username", "username must be less than 8 characters").isLength({
+      max: 8,
+    }),
+    body("password", "password is not valid")
+      .isLength({ max: 8 })
+      .isAlphanumeric(),
+    postLogIn,
     checkLoggedIn,
     passport.authenticate("local", {
       successRedirect: "/",
       failureRedirect: "/login",
+      failureFlash: true,
     })
   );
 
-router.route("/signup").get(signUP).post(postSignUp);
+router
+  .route("/signup")
+  .get(signUP)
+  .post(
+    body("username")
+      .isLength({ max: 8 })
+      .withMessage("username must be more than 8 charchters")
+      .custom((value) => {
+        return db()
+          .collection("users")
+          .findOne({ username: value })
+          .then((user) => {
+            if (user) return Promise.reject("Email Exist");
+
+            // Indicates the success of this synchronous custom validator
+            return true;
+          });
+      }),
+    body(
+      "password",
+      "Pleas Entert the password with only numbers and thext and at least 8 characters"
+    )
+      .isLength({ max: 8 })
+      .isAlphanumeric(),
+    postSignUp
+  );
 
 router.get("/", checkAuthenticated, index);
 
