eb5b111cf7b2759cddb1742e6e40e1f280325ab1
osamaelshaer <osamaelshaer944@gmail.com>
2022-10-10 14:40:09 +0200

    testing by jest

---
+++ __test__/integration/auth.test.js
@@ -0,0 +1,33 @@
+const { User } = require("../../models/User");
+const { dbConnection,client } = require("../../config/database");
+
+beforeAll(async () => {
+  await dbConnection();
+  User.deleteMany();
+});
+
+describe("user auth", () => {
+  let userId;
+  it("should create user ", async () => {
+    new User("osama", "O123456").save();
+    let users = await User.fetchAll();
+    userId = users[0]._id;
+    expect(users.length).toBe(1);
+    expect(users[0]).toMatchObject({ username: "osama" });
+  });
+
+  it("should be find user by name", async () => {
+    let user = await User.findByName("osama");
+    expect(user).toMatchObject({ username: "osama" });
+  });
+
+  it("should be find user by ID", async () => {
+    let user = await User.findById(userId);
+    expect(user).toMatchObject({ username: "osama" });
+  });
+});
+
+afterAll(async () => {
+  await User.deleteMany();
+  await client.close()
+});
